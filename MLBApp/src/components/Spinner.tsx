15c28b3dc97f55b2336bd3a2ebb2a4f37b15ed6a
0112Derrick <0112derrick@gmail.com>
2025-08-08 13:44:41 -0500

    new components

--- MLBApp/src/components/Spinner.tsx
+++ MLBApp/src/components/Spinner.tsx
@@ -1,3 +1,3 @@
 export const Spinner: React.FC = () => (
-  <div className="animate-spin h-8 w-8 border-4 border-t-blue-500 border-gray-200 rounded-full"></div>
+  <div className="animate-spin h-20 w-20 border-4 border-t-blue-500 border-gray-200 rounded-full"></div>
 );
