5d47e2f00fde2fa24567766baf5df55df50425b0
0112Derrick <0112derrick@gmail.com>
2025-08-11 15:47:48 -0500

    Added date picker via shadcn

---
+++ MLBApp/src/components/DatePicker.tsx
@@ -0,0 +1,51 @@
+import { Calendar } from "src/@/components/ui/calendar";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "src/@/components/ui/popover";
+import { Button } from "src/@/components/ui/button";
+import { useState } from "react";
+import { Label } from "src/@/components/ui/label";
+import { ChevronDownIcon } from "lucide-react";
+
+export default function DatePicker({
+  date,
+  setDate,
+}: {
+  date: Date | undefined;
+  setDate: (arg: string | undefined | Date) => void;
+}) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="flex flex-col gap-3">
+      <Label htmlFor="date" className="px-1 font-semibold">
+        Select a date
+      </Label>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            id="date"
+            className="w-48 justify-between font-normal text-black"
+          >
+            {date ? date.toLocaleDateString() : "Select date"}
+            <ChevronDownIcon />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto overflow-hidden p-0" align="start">
+          <Calendar
+            mode="single"
+            selected={date}
+            captionLayout="dropdown"
+            onSelect={(date) => {
+              setDate(date);
+              setOpen(false);
+            }}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+}
