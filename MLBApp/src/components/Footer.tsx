d49c030b7a0c7a529ad8eba4f2378ab8323d4323
0112Derrick <0112derrick@gmail.com>
2025-08-15 17:03:49 -0500

    updated footer to shrink to available space

--- MLBApp/src/components/Footer.tsx
+++ MLBApp/src/components/Footer.tsx
@@ -1,8 +1,6 @@
-import React from "react";
-
 const Footer = () => {
   return (
-    <footer className="text-center py-4 mb-2 bg-gray-900 text-white w-full">
+    <footer className="flex-shrink text-center py-4 mb-2 bg-gray-900 text-white w-full">
       <hr className="opacity-25 p-4 w-full"></hr>
       <span>© 2024 Home Run Hub. All rights reserved.</span>
     </footer>
