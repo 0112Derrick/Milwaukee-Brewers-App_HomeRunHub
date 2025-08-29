d3a1177e8d4b0798aab36e2d18905e4cccec7c89
0112Derrick <0112derrick@gmail.com>
2025-08-28 22:01:11 -0500

    Updated nav styling fixing home button area being wider than the img

--- MLBApp/src/components/NavBar.tsx
+++ MLBApp/src/components/NavBar.tsx
@@ -12,7 +12,7 @@ import { useNavigate } from "react-router-dom";
 const NavBar = () => {
   const navigate = useNavigate();
   return (
-    <div className="flex flex-col flex-shrink opacity-75 hover:opacity-95 z-20 backdrop-blur-lg w-full">
+    <div className="flex flex-col w-full min-h-fit opacity-75 overflow-hidden hover:opacity-95 z-20 backdrop-blur-lg">
       <nav className="flex items-center justify-between bg-gray-900 px-6 py-2 w-full text-white ">
         <div
           className="font-bold text-xl flex gap-2 items-center cursor-pointer"
