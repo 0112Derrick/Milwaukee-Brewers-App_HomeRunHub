80f6d8fe055734672448c0c0e73726eb8e9e1d90
0112Derrick <0112derrick@gmail.com>
2025-08-28 22:09:30 -0500

    Added divisions enum

---
+++ Server/interfaces/enums.ts
@@ -0,0 +1,8 @@
+export enum DivisionsEnum {
+  AmericanLeagueWest = 200,
+  AmericanLeagueEast = 201,
+  AmericanLeagueCentral = 202,
+  NationalLeagueWest = 203,
+  NationalLeagueEast = 204,
+  NationalLeagueCentral = 205,
+}
