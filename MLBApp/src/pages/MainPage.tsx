5592eb5301fa235d206908752a63ba5847e7205b
0112Derrick <0112derrick@gmail.com>
2025-09-07 17:41:19 -0500

    Changed design of header section to flex box with video side by side with header info.

--- MLBApp/src/pages/MainPage.tsx
+++ MLBApp/src/pages/MainPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
-import TitleSection from "../components/TitleSection";
 import { TeamsContainer } from "src/components/TeamsContainer";
+import TitleSection from "src/components/TitleSection";
 
 const MainPage = () => {
   const [prefersReducedMotion, setPrefersReducedMotion] = useState(false);
@@ -31,41 +31,43 @@ const MainPage = () => {
 
   return (
     <div className="flex flex-col flex-grow bg-inherit min-h-0 p-4 overflow-auto">
-      <TitleSection forwardRef={teamSectionRef}></TitleSection>
-
-      <div className="p-4 px-8 flex flex-col items-center justify-center">
-        {!prefersReducedMotion ? (
-          <video
-            ref={video}
-            muted={true}
-            onVolumeChange={() => {
-              if (
-                video &&
-                video.current &&
-                video.current.volume === 1 &&
-                !videoUnmuted
-              ) {
-                video.current.volume = 0.1;
-                videoUnmuted = true;
-              }
-            }}
-            controls
-            preload={"true"}
-            autoPlay={true}
-            loop={true}
-            className="rounded-md min-w-[150px] max-w-[80%] scale-150 px-4 sm:scale-100 sm:mt-12 sm:max-w-[80%]"
-          >
-            <source src={videoSrc} type="video/mp4"></source>
-            Your Browser does not support video tag.
-          </video>
-        ) : (
-          <img
-            src={reducedMotionWorldCupImgSrc}
-            alt="Baseball World Cup"
-            className="aspect-square max-w-[50%] rounded-full"
-          ></img>
-        )}
+      <div className="flex gap-2 sm:flex-col lg:flex-row">
+        <div className="p-4 px-8 flex flex-col items-center justify-center">
+          {!prefersReducedMotion ? (
+            <video
+              ref={video}
+              muted={true}
+              onVolumeChange={() => {
+                if (
+                  video &&
+                  video.current &&
+                  video.current.volume === 1 &&
+                  !videoUnmuted
+                ) {
+                  video.current.volume = 0.1;
+                  videoUnmuted = true;
+                }
+              }}
+              controls
+              preload={"true"}
+              autoPlay={true}
+              loop={true}
+              className="rounded-md sm:w-1/2 sm:aspect-video lg:w-full"
+            >
+              <source src={videoSrc} type="video/mp4"></source>
+              Your Browser does not support video tag.
+            </video>
+          ) : (
+            <img
+              src={reducedMotionWorldCupImgSrc}
+              alt="Baseball World Cup"
+              className="aspect-square max-w-[50%] rounded-full"
+            ></img>
+          )}
+        </div>
+        <TitleSection forwardRef={teamSectionRef}></TitleSection>
       </div>
+
       <TeamsContainer teamSectionRef={teamSectionRef}></TeamsContainer>
     </div>
   );
