b457e67292588ef5dea42b3dc520e3796f627b5e
0112Derrick <0112derrick@gmail.com>
2025-09-07 17:39:07 -0500

    Updated copyright

--- MLBApp/src/components/Footer.tsx
+++ MLBApp/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="flex-shrink flex flex-col items-center p-4 justify-center bg-gray-900 text-white w-full border-t-2 border-t-white">
-      <span>© 2024 Home Run Hub. All rights reserved.</span>
+      <span>© 2025 Home Run Hub. All rights reserved.</span>
     </footer>
   );
 };
