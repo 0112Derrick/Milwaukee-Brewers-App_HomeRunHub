01b3e9383e3305de3043145319b203d2d5078b28
0112Derrick <0112derrick@gmail.com>
2025-09-09 09:38:50 -0500

    Added option to make the team logo name a link or not.

--- MLBApp/src/components/TeamLogoName.tsx
+++ MLBApp/src/components/TeamLogoName.tsx
@@ -5,34 +5,58 @@ export function TeamLogoName({
   id,
   logo,
   teamName,
+  isLink = true,
 }: {
   id: number;
   logo: string;
   teamName: string;
+  isLink?: boolean;
 }) {
   const screenSize = useScreenSize();
-
-  return (
-    <Link
-      to={`/teams/${id}`}
-      className="focus:text-blue-400 hover:text-blue-400"
-    >
-      {screenSize.width > 600 ? (
-        <div className="grid grid-cols-[40%_40%]">
+  if (isLink) {
+    return (
+      <Link
+        to={`/teams/${id}`}
+        className="focus:text-blue-400 hover:text-blue-400"
+      >
+        {screenSize.width > 600 ? (
+          <div className="grid grid-cols-[40%_40%]">
+            <img
+              src={logo}
+              alt={`${teamName} logo`}
+              className="h-8 w-8 justify-self-center"
+            />
+            {teamName}
+          </div>
+        ) : (
           <img
             src={logo}
             alt={`${teamName} logo`}
             className="h-8 w-8 justify-self-center"
           />
-          {teamName}
-        </div>
-      ) : (
-        <img
-          src={logo}
-          alt={`${teamName} logo`}
-          className="h-8 w-8 justify-self-center"
-        />
-      )}
-    </Link>
-  );
+        )}
+      </Link>
+    );
+  } else {
+    return (
+      <div>
+        {screenSize.width > 600 ? (
+          <div className="grid grid-cols-[40%_40%]">
+            <img
+              src={logo}
+              alt={`${teamName} logo`}
+              className="h-8 w-8 justify-self-center"
+            />
+            {teamName}
+          </div>
+        ) : (
+          <img
+            src={logo}
+            alt={`${teamName} logo`}
+            className="h-8 w-8 justify-self-center"
+          />
+        )}
+      </div>
+    );
+  }
 }
