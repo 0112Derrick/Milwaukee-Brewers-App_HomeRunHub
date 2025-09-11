b39d7fd47f70d83fcfdf570d89581fcf4d0e381b
0112Derrick <0112derrick@gmail.com>
2025-09-11 10:20:05 -0500

    Matched styling of nav menu

--- MLBApp/src/components/Footer.tsx
+++ MLBApp/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="flex-shrink flex flex-col items-center p-4 justify-center bg-gray-900 text-white w-full border-t-2 border-t-white">
+    <footer className="flex-shrink flex flex-col items-center p-4 justify-center bg-gray-900 text-white w-full border-t border-t-white/25">
       <span>© 2025 Home Run Hub. All rights reserved.</span>
     </footer>
   );
